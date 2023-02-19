d8076d09c84fe6c105cb2b9a45ca3dd0c001bb21
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-19 12:55:37 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,20 +15,16 @@ int main(void)
 
         for (a = 0; a <= 9; a++)
         {
-                for (b = a + 1; b <= 9; b++)
+		for (b = a + 1; b <= 9; b++)
                 {
 			for (c = b + 1; c <= 9; c++)
 			{
-				if (a != b)
+				if (a != b != c)
 				{
 					putchar('0' + a);
                                		putchar('0' + b);
 					putchar('0' + c);
                        		 }
-                        	if (a == 7 && b == 8 && c == 9)
-                        	{
-                                	continue;
-                        	}
 			}
 
                         putchar(',');
