ccc02dc4d38b378497376a563575276318d4175f
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-19 14:02:09 -0800

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,18 +16,18 @@ int main(void)
 	while (a < 10)
 	{
 		a++;
+		putchar('0' + a0);
 
 		while (b < 10)
 		{
 			b++;
+			putchar('0' + b);
 
 			while (c < 10)
 			{
 				c++;
-			}
-				putchar('0' + a);
-				putchar('0' + b);
 				putchar('0' + c);
+			}
 		}
 	}
 	
