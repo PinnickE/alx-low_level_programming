84f4315e717c74819b75162bd46e89780d73f38d
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-21 14:22:08 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet ten times
+ *
+ * Return: 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int cntr = 0;
+
+	while (cntr <= 10)
+	{
+		int ltr;
+
+		for (ltr = 'a'; ltr <= 'z'; ltr++)
+		{
+			_putchar(ltr);
+		}
+	cntr++;
+	}
+
+	_putchar('\n');
+}
