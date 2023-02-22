d45d8de19e9e61a867af1bedcd580299d32cecc4
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-21 18:19:47 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @int: first integer
+ *
+ * Return: 0
+ *
+ */
+
+int add(int, int)
+{
+	int a;
+	int b;
+	int c;
+
+	c = a + b;
+
+	_putchar(c + '0')';
+}
