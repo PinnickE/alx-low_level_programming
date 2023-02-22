62520ef9de669028291092f8b064030206452bab
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-21 16:12:47 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,6 +3,8 @@
 /**
  * print_sign - print the sign of a number
  *
+ * @n: Integer to parse
+ *
  * Return: Always 0 (Success)
  */
 
@@ -11,12 +13,12 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar("+1");
+		_putchar(43);
 	}
 	else if (n < 0)
 	{
 		return (-1);
-		_putchar("-1");
+		_putchar(45);
 	}
 	else
 	{
