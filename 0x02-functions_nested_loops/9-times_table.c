f0bf3443b724654f686ec42f91118565b3b310da
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-22 09:56:15 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,18 +14,22 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
+		_putchar(48);
 
 		for (y = 1; y <= 9; y++)
 		{
-			z = (y * x);
+			_putchar(44);
+			_putchar(32);
 
-			_putchar((z / 10) + '0');
-			/*_putchar((z % 10) + '0');*/
-			if (y <= 9)
+			z = y * x;
+			
+			if (z <= 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				_putchar(32);
 			}
+			else
+				_putchar((z / 10) + 0);
+
 			_putchar((z % 10) + '0');
 		}
 
