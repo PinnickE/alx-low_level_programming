db1cdce7703c4f40fedeca2052a7a7b4bb8be899
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-23 09:29:38 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - multiplying two integers
+ *
+ * @a: first integer to parse
+ * @b: second integer to parse
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	_puthcar(result);
+
+	return (0);
+}
