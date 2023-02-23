af58335238de05532a370b7c0cf9bd4ad68bc3e3
Emmanuel <emmanuelpinnick@gmail.com>
2023-02-23 09:12:51 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - Checking for uppercase characters
+ *
+ * @c: character to parse
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if ((c > 64) && (c < 90))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
