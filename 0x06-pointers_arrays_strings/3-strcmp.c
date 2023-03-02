9256fd5ca2f58ca9c03d616fe0b0680522b87242
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-02 14:58:53 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,11 +14,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int a = 0;
 
-	while (s1[a] == s2[a] && s1[a] != '\0' && s2[a] != '\0')
+	while (s1[a] != '\0' && s2[a] != '\0')
 	{
-		return (0);
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
 		a++;
 	}
 
-	return (s1[a] - s2[a]);
+	return (0);
 }
