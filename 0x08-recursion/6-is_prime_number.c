96bb8b5ae8c30c1d163b60d1ae91eb955bcd4659
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-08 17:46:46 -0800

    Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,24 +6,49 @@
  *
  * @n: Number to check
  *
- * Return 1 or 0
+ * @x: Possible primes t check
+ *
+ * Return: 1 or 0
  *
  */
+int primer(int n,int x);
 
 int is_prime_number(int n)
 {
-	int x;
-
-	if (n <= 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (n == 1)
-	{
-		return (1);
-	}	
 	else
 	{
-		return (n % (is_prime_number(n - 1));
+		return (primer(n, 2));
+	}
+}
+
+/**
+ * primer - Check numbers
+ *
+ * @n: Number
+ * @x: Prime
+ *
+ * Return: Result
+ *
+ */
+int primer(int n, int x)
+{
+	if (n % x == 0)
+	{
+		if (x == n)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (primer(n, x + 1));
 	}
 }
