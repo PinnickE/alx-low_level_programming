11e26b832acc49d02c9ff86192f8943f3b01ad61
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-15 12:13:52 -0700

    To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,8 +14,7 @@
 
 int main(int argc, char *argv[])
 {
-	int a, b, x;
-	int c = 0;
+	int a, b, c, x;
 	int sum = 0;
 
 	if (argc == 1)
@@ -25,14 +24,13 @@ int main(int argc, char *argv[])
 
 	for (b = 1; b < argc; b++)
 	{
-		while (argv[b][c] != '\0')
+		for (c = 0; argv[b][c] != '\0'; c++)
 		{
 			if (argv[b][c] < 48 || argv[b][c] > 57)
 			{
 				printf("%s\n", "Error");
 				return (1);
 			}
-			c++;
 		}
 	}
 
