c96970acf34803f759f874560c6cfafa708d8f4a
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-20 11:31:05 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - A dog structure
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Owner's name
+ *
+ * Description: A struct to help initialise
+ * an instance of a dog
+ *
+ */
+
 struct dog
 {
 	char *name;
