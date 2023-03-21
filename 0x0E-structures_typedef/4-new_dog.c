bfab26575c31a9e8d68239959a558fb5bda600f7
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-20 18:36:04 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -35,6 +35,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		len1++;
 	}
+	len1++;
 
 	(*dog).name = malloc(sizeof(char) * len1);
 	
@@ -56,6 +57,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		len2++;
 	}
+	len2++;
 
 	(*dog).owner = malloc(sizeof(char) * len2);
 
