7a00b16ba0b9923cbe4c5cf49baa6112c4b4e598
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-20 18:41:47 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,77 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
 
 /**
  * new_dog - Create a new dog
- *
  * @name: Name of dog
  * @age: Age of dog
  * @owner: Name of owner
- *
  * Return: Pointer to dog, or NULL if function fails
- *
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int len1, len2, cntr;
-
 	dog_t *dog;
 
 	if (name == NULL || owner == NULL)
-	{
 		return (NULL);
-	}
-
 	dog = malloc(sizeof(dog_t));
-
 	if (dog == NULL)
-	{
 		return (NULL);
-	}
 	len1 = 0;
 	while (name[len1] != '\0')
-	{
 		len1++;
-	}
 	len1++;
-
 	(*dog).name = malloc(sizeof(char) * len1);
-	
 	if ((*dog).name == NULL)
-	{
 		free(dog);
 		return (NULL);
-	}
-
 	for (cntr = 0; cntr < len1; cntr++)
-	{
 		(*dog).name[cntr] = name[cntr];
-	}
-
 	(*dog).age = age;
-
 	len2 = 0;
 	while (owner[len2] != '\0')
-	{
 		len2++;
-	}
 	len2++;
-
 	(*dog).owner = malloc(sizeof(char) * len2);
-
 	if ((*dog).owner == NULL)
-	{
 		free((*dog).name);
 		free(dog);
 		return (NULL);
-	}
-
 	for (cntr = 0; cntr < len2; cntr++)
-	{
 		(*dog).owner[cntr] = owner[cntr];
-	}
-
 	return (dog);
 }
