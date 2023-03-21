48f3b91b47eadde16418576b25d585c1405ed30d
Emmanuel <emmanuelpinnick@gmail.com>
2023-03-20 18:11:36 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,53 +26,50 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 
-	if (dog)
-	{
-		len1 = 0;
-		while (name[len1] != '\0')
-		{
-			len1++;
-		}
-
-		(*dog).name = malloc(sizeof(char) * len1);
-
-		if ((*dog).name == NULL)
-		{
-			free(dog);
-			return (NULL);
-		}
-
-		for (cntr = 0; cntr < len1; cntr++)
-		{
-			(*dog).name[cntr] = name[cntr];
-		}
-
-		(*dog).age = age;
-
-		len2 = 0;
-		while (owner[len2] != '\0')
-		{
-			len2++;
-		}
-
-		(*dog).owner = malloc(sizeof(char) * len2);
-
-		if ((*dog).owner == NULL)
-		{
-			free((*dog).name);
-			free(dog);
-			return (NULL);
-		}
-
-		for (cntr = 0; cntr < len2; cntr++)
-		{
-			(*dog).owner[cntr] = owner[cntr];
-		}
-
-		return (dog);
-	}
-	else
+	if (dog == NULL)
 	{
 		return (NULL);
 	}
+	len1 = 0;
+	while (name[len1] != '\0')
+	{
+		len1++;
+	}
+
+	(*dog).name = malloc(sizeof(char) * len1);
+	
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	for (cntr = 0; cntr < len1; cntr++)
+	{
+		(*dog).name[cntr] = name[cntr];
+	}
+
+	(*dog).age = age;
+
+	len2 = 0;
+	while (owner[len2] != '\0')
+	{
+		len2++;
+	}
+
+	(*dog).owner = malloc(sizeof(char) * len2);
+
+	if ((*dog).owner == NULL)
+	{
+		free((*dog).name);
+		free(dog);
+		return (NULL);
+	}
+
+	for (cntr = 0; cntr < len2; cntr++)
+	{
+		(*dog).owner[cntr] = owner[cntr];
+	}
+
+	return (dog);
 }
