95b0befc1fa054654744b170742fab240dfeed0d
PinnickE <emmanuelpinnick@gmail.com>
2023-03-30 18:00:29 -0700

    Extremely annoying stuff

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stddef.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,12 +9,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct lisit_s *next;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
